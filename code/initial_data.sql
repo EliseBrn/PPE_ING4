63bd203ffc74c5a92c3271f7062f89f67e36411b
Elise <148560661+EliseBrn@users.noreply.github.com>
2025-02-01 11:06:04 +0100

    Create initial_data.sql

---
+++ code/initial_data.sql
@@ -0,0 +1,3 @@
+-- Insertion de l'utilisateur anonyme
+INSERT INTO user (id_user, email, password, first_name, pseudo)
+VALUES (0, 'anonymous@example.com', 'not_applicable', 'Anonymous', 'Anonymous');
